5d66db6c9f1038b9d65c203c4100781b329c56af
MiledAmir <amirmiled23@gmail.com>
2025-04-30 01:23:32 +0100

    crud,event/categorie,api

---
+++ src/Entity/ReservationMateriel.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+use App\Repository\ReservationMaterielRepository;
+
+#[ORM\Entity(repositoryClass: ReservationMaterielRepository::class)]
+#[ORM\Table(name: 'reservation_materiel')]
+class ReservationMateriel
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id_reservation = null;
+
+    public function getId_reservation(): ?int
+    {
+        return $this->id_reservation;
+    }
+
+    public function setId_reservation(int $id_reservation): self
+    {
+        $this->id_reservation = $id_reservation;
+        return $this;
+    }
+
+    #[ORM\Column(type: 'date', nullable: false)]
+    private ?\DateTimeInterface $date_debut = null;
+
+    public function getDate_debut(): ?\DateTimeInterface
+    {
+        return $this->date_debut;
+    }
+
+    public function setDate_debut(\DateTimeInterface $date_debut): self
+    {
+        $this->date_debut = $date_debut;
+        return $this;
+    }
+
+    #[ORM\Column(type: 'date', nullable: false)]
+    private ?\DateTimeInterface $date_fin = null;
+
+    public function getDate_fin(): ?\DateTimeInterface
+    {
+        return $this->date_fin;
+    }
+
+    public function setDate_fin(\DateTimeInterface $date_fin): self
+    {
+        $this->date_fin = $date_fin;
+        return $this;
+    }
+
+    #[ORM\Column(type: 'integer', nullable: false)]
+    private ?int $quantite_reservee = null;
+
+    public function getQuantite_reservee(): ?int
+    {
+        return $this->quantite_reservee;
+    }
+
+    public function setQuantite_reservee(int $quantite_reservee): self
+    {
+        $this->quantite_reservee = $quantite_reservee;
+        return $this;
+    }
+
+    #[ORM\Column(type: 'integer', nullable: false)]
+    private ?int $id_materiel = null;
+
+    public function getId_materiel(): ?int
+    {
+        return $this->id_materiel;
+    }
+
+    public function setId_materiel(int $id_materiel): self
+    {
+        $this->id_materiel = $id_materiel;
+        return $this;
+    }
+
+    public function getIdReservation(): ?int
+    {
+        return $this->id_reservation;
+    }
+
+    public function getDateDebut(): ?\DateTime
+    {
+        return $this->date_debut;
+    }
+
+    public function setDateDebut(\DateTime $date_debut): static
+    {
+        $this->date_debut = $date_debut;
+
+        return $this;
+    }
+
+    public function getDateFin(): ?\DateTime
+    {
+        return $this->date_fin;
+    }
+
+    public function setDateFin(\DateTime $date_fin): static
+    {
+        $this->date_fin = $date_fin;
+
+        return $this;
+    }
+
+    public function getQuantiteReservee(): ?int
+    {
+        return $this->quantite_reservee;
+    }
+
+    public function setQuantiteReservee(int $quantite_reservee): static
+    {
+        $this->quantite_reservee = $quantite_reservee;
+
+        return $this;
+    }
+
+    public function getIdMateriel(): ?int
+    {
+        return $this->id_materiel;
+    }
+
+    public function setIdMateriel(int $id_materiel): static
+    {
+        $this->id_materiel = $id_materiel;
+
+        return $this;
+    }
+
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,381 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+use App\Repository\UserRepository;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: 'user')]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id_user = null;
+
+    public function getId_user(): ?int
+    {
+        return $this->id_user;
+    }
+
+    public function setId_user(int $id_user): self
+    {
+        $this->id_user = $id_user;
+        return $this;
+    }
+
+    #[ORM\Column(type: 'string', nullable: false)]
+    private ?string $nom_user = null;
+
+    public function getNom_user(): ?string
+    {
+        return $this->nom_user;
+    }
+
+    public function setNom_user(string $nom_user): self
+    {
+        $this->nom_user = $nom_user;
+        return $this;
+    }
+
+    #[ORM\Column(type: 'string', nullable: false)]
+    private ?string $prenom_user = null;
+
+    public function getPrenom_user(): ?string
+    {
+        return $this->prenom_user;
+    }
+
+    public function setPrenom_user(string $prenom_user): self
+    {
+        $this->prenom_user = $prenom_user;
+        return $this;
+    }
+
+    #[ORM\Column(type: 'string', nullable: false)]
+    private ?string $email_user = null;
+
+    public function getEmail_user(): ?string
+    {
+        return $this->email_user;
+    }
+
+    public function setEmail_user(string $email_user): self
+    {
+        $this->email_user = $email_user;
+        return $this;
+    }
+
+    #[ORM\Column(type: 'integer', nullable: false)]
+    private ?int $num_user = null;
+
+    public function getNum_user(): ?int
+    {
+        return $this->num_user;
+    }
+
+    public function setNum_user(int $num_user): self
+    {
+        $this->num_user = $num_user;
+        return $this;
+    }
+
+    #[ORM\Column(type: 'string', nullable: false)]
+    private ?string $adresse_user = null;
+
+    public function getAdresse_user(): ?string
+    {
+        return $this->adresse_user;
+    }
+
+    public function setAdresse_user(string $adresse_user): self
+    {
+        $this->adresse_user = $adresse_user;
+        return $this;
+    }
+
+    #[ORM\Column(type: 'integer', nullable: false)]
+    private ?int $age_user = null;
+
+    public function getAge_user(): ?int
+    {
+        return $this->age_user;
+    }
+
+    public function setAge_user(int $age_user): self
+    {
+        $this->age_user = $age_user;
+        return $this;
+    }
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private ?int $point_user = null;
+
+    public function getPoint_user(): ?int
+    {
+        return $this->point_user;
+    }
+
+    public function setPoint_user(?int $point_user): self
+    {
+        $this->point_user = $point_user;
+        return $this;
+    }
+
+    #[ORM\Column(type: 'string', nullable: false)]
+    private ?string $pw_user = null;
+
+    public function getPw_user(): ?string
+    {
+        return $this->pw_user;
+    }
+
+    public function setPw_user(string $pw_user): self
+    {
+        $this->pw_user = $pw_user;
+        return $this;
+    }
+
+    #[ORM\Column(type: 'date', nullable: false)]
+    private ?\DateTimeInterface $date_inscri = null;
+
+    public function getDate_inscri(): ?\DateTimeInterface
+    {
+        return $this->date_inscri;
+    }
+
+    public function setDate_inscri(\DateTimeInterface $date_inscri): self
+    {
+        $this->date_inscri = $date_inscri;
+        return $this;
+    }
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private ?int $reset_code = null;
+
+    public function getReset_code(): ?int
+    {
+        return $this->reset_code;
+    }
+
+    public function setReset_code(?int $reset_code): self
+    {
+        $this->reset_code = $reset_code;
+        return $this;
+    }
+
+    #[ORM\OneToMany(targetEntity: Aviss::class, mappedBy: 'user')]
+    private Collection $avisss;
+
+    /**
+     * @return Collection<int, Aviss>
+     */
+    public function getAvisss(): Collection
+    {
+        if (!$this->avisss instanceof Collection) {
+            $this->avisss = new ArrayCollection();
+        }
+        return $this->avisss;
+    }
+
+    public function addAviss(Aviss $aviss): self
+    {
+        if (!$this->getAvisss()->contains($aviss)) {
+            $this->getAvisss()->add($aviss);
+        }
+        return $this;
+    }
+
+    public function removeAviss(Aviss $aviss): self
+    {
+        $this->getAvisss()->removeElement($aviss);
+        return $this;
+    }
+
+    #[ORM\OneToMany(targetEntity: Userprofil::class, mappedBy: 'user')]
+    private Collection $userprofils;
+
+    public function __construct()
+    {
+        $this->avisss = new ArrayCollection();
+        $this->userprofils = new ArrayCollection();
+    }
+
+    /**
+     * @return Collection<int, Userprofil>
+     */
+    public function getUserprofils(): Collection
+    {
+        if (!$this->userprofils instanceof Collection) {
+            $this->userprofils = new ArrayCollection();
+        }
+        return $this->userprofils;
+    }
+
+    public function addUserprofil(Userprofil $userprofil): self
+    {
+        if (!$this->getUserprofils()->contains($userprofil)) {
+            $this->getUserprofils()->add($userprofil);
+        }
+        return $this;
+    }
+
+    public function removeUserprofil(Userprofil $userprofil): self
+    {
+        $this->getUserprofils()->removeElement($userprofil);
+        return $this;
+    }
+
+    public function getIdUser(): ?int
+    {
+        return $this->id_user;
+    }
+
+    public function getNomUser(): ?string
+    {
+        return $this->nom_user;
+    }
+
+    public function setNomUser(string $nom_user): static
+    {
+        $this->nom_user = $nom_user;
+
+        return $this;
+    }
+
+    public function getPrenomUser(): ?string
+    {
+        return $this->prenom_user;
+    }
+
+    public function setPrenomUser(string $prenom_user): static
+    {
+        $this->prenom_user = $prenom_user;
+
+        return $this;
+    }
+
+    public function getEmailUser(): ?string
+    {
+        return $this->email_user;
+    }
+
+    public function setEmailUser(string $email_user): static
+    {
+        $this->email_user = $email_user;
+
+        return $this;
+    }
+
+    public function getNumUser(): ?int
+    {
+        return $this->num_user;
+    }
+
+    public function setNumUser(int $num_user): static
+    {
+        $this->num_user = $num_user;
+
+        return $this;
+    }
+
+    public function getAdresseUser(): ?string
+    {
+        return $this->adresse_user;
+    }
+
+    public function setAdresseUser(string $adresse_user): static
+    {
+        $this->adresse_user = $adresse_user;
+
+        return $this;
+    }
+
+    public function getAgeUser(): ?int
+    {
+        return $this->age_user;
+    }
+
+    public function setAgeUser(int $age_user): static
+    {
+        $this->age_user = $age_user;
+
+        return $this;
+    }
+
+    public function getPointUser(): ?int
+    {
+        return $this->point_user;
+    }
+
+    public function setPointUser(?int $point_user): static
+    {
+        $this->point_user = $point_user;
+
+        return $this;
+    }
+
+    public function getPwUser(): ?string
+    {
+        return $this->pw_user;
+    }
+
+    public function setPwUser(string $pw_user): static
+    {
+        $this->pw_user = $pw_user;
+
+        return $this;
+    }
+
+    public function getDateInscri(): ?\DateTime
+    {
+        return $this->date_inscri;
+    }
+
+    public function setDateInscri(\DateTime $date_inscri): static
+    {
+        $this->date_inscri = $date_inscri;
+
+        return $this;
+    }
+
+    public function getResetCode(): ?int
+    {
+        return $this->reset_code;
+    }
+
+    public function setResetCode(?int $reset_code): static
+    {
+        $this->reset_code = $reset_code;
+
+        return $this;
+    }
+
+    public function addAvisss(Aviss $avisss): static
+    {
+        if (!$this->avisss->contains($avisss)) {
+            $this->avisss->add($avisss);
+            $avisss->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAvisss(Aviss $avisss): static
+    {
+        if ($this->avisss->removeElement($avisss)) {
+            // set the owning side to null (unless already changed)
+            if ($avisss->getUser() === $this) {
+                $avisss->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+}
